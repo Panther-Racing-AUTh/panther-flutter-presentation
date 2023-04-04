878a3fb04939ff285d8afdca8586201cdf58b406
kalaitzisxristos <kalaitzisxristos@hotmail.com>
2023-04-05 01:40:17 +0300

    add step 3

---
+++ step_3/README.md
@@ -0,0 +1,28 @@
+# step_3
+
+The project is a simple counter app with a button that increases a counter by one when pressed.
+
+## Prerequisites
+- Flutter SDK (>= 2.0.0)
+- Android Studio or VS Code with Flutter extension
+
+## Getting Started
+1. Clone the repository or download the ZIP file.
+2. Open the project in Android Studio or VS Code.
+3. Install the dependencies by running `flutter pub get` in the terminal.
+4. Run the app with `flutter run` command in the terminal or use the Run button in the IDE.
+
+## Project Structure
+- `main.dart` file contains the code for the `MyApp` widget which is the root of the app.
+- `MyHomePage` widget is the home page of the app and it contains the code for the counter and the button that increments it.
+- `pubspec.yaml` file lists the dependencies used in the project.
+
+## How to Use
+1. Launch the app.
+2. Press the button to increment the counter by one.
+3. The number of button presses is displayed on the screen.
+
+## Notes
+- The app has a blue theme with a panther blue color for the primary color.
+- The app uses the `setState` function to update the counter and re-render the screen.
+- The `main.dart` file also contains commented-out code for changing the primary color and resetting the counter.
---
+++ step_3/lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      // title: 'Panther Racing AUTh',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // primarySwatch: MaterialColor(pantherColor.value, color),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: const MyHomePage(title: 'ΣΦΗΜΜΥ 14'),
+    );
+  }
+}
+
+final pantherColor = Color(0xFF066791);
+Map<int, Color> color =
+{
+  50: Color((pantherColor.value & 0xFFFFFF) | 0x08000000),
+  100: Color((pantherColor.value & 0xFFFFFF) | 0x14000000),
+  200: Color((pantherColor.value & 0xFFFFFF) | 0x1F000000),
+  300: Color((pantherColor.value & 0xFFFFFF) | 0x2B000000),
+  400: Color((pantherColor.value & 0xFFFFFF) | 0x38000000),
+  500: Color((pantherColor.value & 0xFFFFFF) | 0x45000000),
+  600: Color((pantherColor.value & 0xFFFFFF) | 0x51000000),
+  700: Color((pantherColor.value & 0xFFFFFF) | 0x5E000000),
+  800: Color((pantherColor.value & 0xFFFFFF) | 0x6A000000),
+  900: Color((pantherColor.value & 0xFFFFFF) | 0x77000000),
+};
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+      // if (_counter == 5) _counter = 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+              // style: TextStyle(fontSize: 40),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+        // child: const Icon(Icons.flutter_dash_sharp),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
